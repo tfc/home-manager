97fd78b7f9fb2ed214163d2b958071f70254079d
Jacek Galowicz <jacek@galowicz.de>
2019-05-21 22:58:51 +0200

    Update tmux config for newer tmux version

--- packages/tmux/default.nix
+++ packages/tmux/default.nix
@@ -1,11 +1,16 @@
 # Tmux with ./tmux.conf baked in
-{ tmux, writeText, symlinkJoin, makeWrapper }:
-symlinkJoin {
+{ tmux, tmuxPlugins, writeText, symlinkJoin, makeWrapper }:
+let
+  tmuxConf = (with tmuxPlugins; ''
+    run-shell ${resurrect}/share/tmux-plugins/resurrect/resurrect.tmux
+    run-shell ${continuum}/share/tmux-plugins/continuum/continuum.tmux
+  '') + builtins.readFile ./tmux.conf  ;
+in symlinkJoin {
   name = "tmux";
   buildInputs = [makeWrapper];
   paths = [ tmux ];
   postBuild = ''
     wrapProgram "$out/bin/tmux" \
-    --add-flags "-f ${./tmux.conf}"
+    --add-flags "-f ${writeText "tmux.conf" tmuxConf}"
   '';
 }
