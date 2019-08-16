c7c840658fdcc7d8f23205ec0c4cc70e6f66493e
Jacek Galowicz <jacek@galowicz.de>
2019-08-16 12:46:50 +0200

    Add vim-cmake

--- packages/neovim/default.nix
+++ packages/neovim/default.nix
@@ -1,4 +1,4 @@
-{ vimUtils, fetchFromGitHub, neovim, vimPlugins }:
+{ cmake, vimUtils, fetchFromGitHub, neovim, vimPlugins }:
 
 let
   emodeline = vimUtils.buildVimPluginFrom2Nix {
@@ -10,6 +10,19 @@ let
       rev = "19550795743876c2256021530209d83592f5924a";
       sha256 = "0x9y7rzbk6g8cq6jkn37wi95wzhq0abban6w10652v4kdmjrxrr0";
     };
+  };
+  vim-cmake = vimUtils.buildVimPluginFrom2Nix {
+    pname = "vim-cmake";
+    version = "1.0";
+    src = fetchFromGitHub {
+      owner = "vhdirk";
+      repo = "vim-cmake";
+      rev = "8cc735ba804354c860c8029acf3a5e1539ce711c";
+      sha256 = "166ffq0plpi7wv5n7h5vgayfjm5ld5a18rbmv9gs4n7fan02yz0j";
+    };
+    postPatch = ''
+      sed -i 's;executable("cmake");executable("${cmake}/bin/cmake");' plugin/cmake.vim
+    '';
   };
 
 in neovim.override {
@@ -27,6 +40,7 @@ in neovim.override {
         surround
         stylish-haskell
         vim-airline
+        vim-cmake
         vim-colorschemes
         vim-nix
         vim-tmux-navigator
