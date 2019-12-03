8ff17dcfad940014c569d2482cbbb4b4a017b2d3
Jacek Galowicz <jacek@galowicz.de>
2019-12-03 14:11:01 +0100

    Tidy up darwin

--- configs/darwin.nix
+++ configs/darwin.nix
@@ -4,15 +4,7 @@
   imports = [
     ./common.nix
     ./textmedia.nix
+    ../modules/shell/zsh.nix
+    ../modules/yubikey.nix
   ];
-
-  programs.bash = {
-    sessionVariables = {
-      # Need to test this out
-      #SSH_AUTH_SOCK = "~/.gnupg/S.gpg-agent.ssh";
-    };
-    profileExtra = ''
-      export NIX_PATH=$HOME/.nix-defexpr/channels''${NIX_PATH:+:}$NIX_PATH
-    '';
-  };
 }
