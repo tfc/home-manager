ccb535bbaaf48f7bce630cf21e91d64a201ced55
Jacek Galowicz <jacek@galowicz.de>
2020-01-15 17:36:01 +0100

    Configure htop

--- modules/shelltools.nix
+++ modules/shelltools.nix
@@ -7,7 +7,6 @@ let
   ];
 in {
   home.packages = with pkgs; [
-    htop
     magic-wormhole
     mosh
     nix-top
@@ -20,6 +19,29 @@ in {
   programs = {
     bat.enable = true;
     command-not-found.enable = true;
+    htop = {
+      enable = true;
+      colorScheme = 6;
+      cpuCountFromZero = true;
+      highlightBaseName = true;
+      highlightMegabytes = true;
+      highlightThreads = true;
+      meters = {
+        left =  [
+          "AllCPUs2"
+          "Memory"
+          "Swap"
+          { kind = "Battery"; mode = 1; }
+        ];
+        right = [
+          "Hostname"
+          "Tasks"
+          "LoadAverage"
+          "Uptime"
+          { kind = "CPU"; mode = 3; }
+        ];
+      };
+    };
     fzf = {
       enable = true;
       enableBashIntegration = true;
