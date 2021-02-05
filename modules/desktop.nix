84ddb7d051ff71ab68afe69e471ddb4085a5028b
Jacek Galowicz <jacek@galowicz.de>
2021-02-05 15:44:56 +0100

    Tweak firefox settings

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -30,6 +30,31 @@
     extensions = [];
     enableGnomeExtensions = true;
     package = pkgs.firefox-bin;
+    profiles = {
+      myprofile = {
+        settings = {
+          "browser.sessionstore.interval" = 60000;
+          "browser.tabs.animate" = false;
+          "browser.download.animateNotifications" = false;
+          "browser.urlbar.maxRichResults" = 5;
+          "dom.battery.enabled" = false;
+          "extensions.pocket.enabled" = false;
+          "general.smoothScroll" = false;
+          "network.prefetch-next" = false;
+          "privacy.trackingprotection.enabled" = true;
+          "toolkit.cosmeticAnimations.enabled" = false;
+
+          # performance suggestions from https://gist.github.com/0XDE57/fbd302cef7693e62c769
+          "layout.frame_rate.precise" = true;
+          "webgl.force-enabled" = true;
+          "layers.acceleration.force-enabled" = true;
+          "layers.offmainthreadcomposition.enabled" = true;
+          "layers.offmainthreadcomposition.async-animations" = true;
+          "layers.async-video.enabled" = true;
+          "html5.offmainthread" = true;
+        };
+      };
+    };
   };
 
   programs.chromium = {
