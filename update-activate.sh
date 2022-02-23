7011e8c2e9ecae274b65d33940ff6aee254d9446
Jacek Galowicz <jacek@galowicz.de>
2022-02-23 14:05:44 +0100

    Add more log output to updater script

--- update-activate.sh
+++ update-activate.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 rm -f result
 
 nix flake update
-nix build .#homeManagerConfigurations.linux.activationPackage
+nix -L build .#homeManagerConfigurations.linux.activationPackage --option substituters https://cache.nixos.org
 ./result/bin/home-manager-generation
 
 
